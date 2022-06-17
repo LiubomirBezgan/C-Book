ce5db2c3b3417c771d39a84b901d5d955cad65fb
LiubomirBezgan <106992442+LiubomirBezgan@users.noreply.github.com>
2022-06-17 19:36:54 +0200

    v2.0

--- Chapter 1/cw1_rozdzial1.c
+++ Chapter 1/cw1_rozdzial1.c
@@ -1,3 +1,7 @@
+// C Primer Plus Chapter 1 Exercise 1
+// Program that converts inches to centimeters (1 inch = 2.54 cm)
+// You have to delete certain Single Line Comments ("//") in the main() function in order to run target language version
+// Version 2.0
 #include <stdio.h>
 
 int main(void)
@@ -5,9 +9,21 @@ int main(void)
 	float in;
 	float out;
 
-	printf ("Siema, Byku! \nProsze wprowadzic wartosc w calach:\n");
+	// EN delete next "//" if you going to use English verion
+	//printf ("Hi Big Man! \nPlease enter an inch value:\n");
+
+	// PL usuń następne "//", jeśli zamierzasz użyć wersję polską
+	//printf ("Siema, Byku! \nProsze wprowadzic wartosc w calach:\n");
+
 	scanf("%e", &in);
 	out = in * 2.54;
-	printf ("Wartosc w cm: %e cm", out);
+
+	// EN delete next "//" if you going to use English verion
+	//printf ("The centimeter value equals %e cm", out);
+
+	// PL usuń następne "//", jeśli zamierzasz użyć wersję polską
+	//printf ("Wartosc w cm: %e cm", out);
+
+
 	return 0;
 	}
