b63c6782648ee5a4bd0f41c7ed53dd6a7f8940b9
LiubomirBezgan <106992442+LiubomirBezgan@users.noreply.github.com>
2022-06-22 21:40:04 +0200

    for SKG.Denisqa

---
+++ Chapter 1/cw1_chapter1_for_SKG_Denisqa.c
@@ -0,0 +1,30 @@
+// C Primer Plus Chapter 1 Exercise 1
+// Program that converts inches to centimeters (1 inch = 2.54 cm)
+// You have to delete certain Single Line Comments ("//") in the main() function in order to run target language version
+// Version 2.0
+#include <stdio.h>
+
+int main(void)
+	{
+	float in;
+	float out;
+
+	// EN delete next "//" if you going to use English verion
+	printf ("Hi Big Man! \nPlease enter an inch value:\n");
+
+	// PL usuń następne "//", jeśli zamierzasz użyć wersję polską
+	//printf ("Siema, Byku! \nProsze wprowadzic wartosc w calach:\n");
+
+	scanf("%e", &in);
+	out = in * 2.54;
+
+	// EN delete next "//" if you going to use English verion
+	printf ("The centimeter value equals %e cm", out);
+
+	// PL usuń następne "//", jeśli zamierzasz użyć wersję polską
+	//printf ("Wartosc w cm: %e cm", out);
+
+	while(1)
+	{}
+	return 0;
+	}
