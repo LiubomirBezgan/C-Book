b12dc1f59078f84d688d82e70c7dd96636e9b485
LiubomirBezgan <106992442+LiubomirBezgan@users.noreply.github.com>
2022-07-02 22:46:15 +0200

    v4.0

--- Chapter 3/Programming Exercises/Exercise1Chapter3.c
+++ Chapter 3/Programming Exercises/Exercise1Chapter3.c
@@ -1,10 +1,21 @@
-// wyświetla rozmiary typów
-// v3.0
+// Wyświetla rozmiary typów oraz wyniki dokonan przepełnienia i niedomiaru, wykorzystujac narzedzia do formatowania
+// v4.0
 #include <stdio.h>
 #include <limits.h>	// granice liczb calkowitych
 #include <float.h>	// granice liczb zmiennoprzecinkowych
+#define C "char"
+#define S "short"
+#define I "int"
+#define U "unsigned"
+#define L "long"
+#define LL "long long"
+
 int main(void)
 {
+	// definicja stałych
+	const char *FLT = "float";
+	const char *DBL = "double";
+
 	// definicja zmiennych
 	char char_var;
 	short short_var;
@@ -20,27 +31,29 @@ int main(void)
 	long long long_var1;
 	float float_var1;
 	double double_var1;
-	  
+	
 	// w C99 zdefionowano format %zd dla rozmiarów
-	printf("Typ char \tma rozmiar %zd bajt.\n", sizeof(char));
-	printf("Typ short \tma rozmiar %zd bajtow.\n", sizeof(short));
-	printf("Typ int \tma rozmiar %zd bajtow.\n", sizeof(int));
-	printf("Typ unsigned \tma rozmiar %zd bajtow.\n", sizeof(unsigned));
-	printf("Typ long \tma rozmiar %zd bajtow.\n", sizeof(long));
-	printf("Typ long long \tma rozmiar %zd bajtow.\n", sizeof(long long));
-	printf("Typ float \tma rozmiar %zd bajtow.\n", sizeof(float));
-	printf("Typ double \tma rozmiar %zd bajtow.\n\n", sizeof(double));
+	printf("Typ %9s ma rozmiar %zd bajt\n", C, sizeof(char));
+	printf("Typ %9s ma rozmiar %zd bajtow\n", S, sizeof(short));
+	printf("Typ %9s ma rozmiar %zd bajtow\n", I, sizeof(int));
+	printf("Typ %9s ma rozmiar %zd bajtow\n", U, sizeof(unsigned));
+	printf("Typ %9s ma rozmiar %zd bajtow\n", L, sizeof(long));
+	printf("Typ %9s ma rozmiar %zd bajtow\n", LL, sizeof(long long));
+	printf("Typ %9s ma rozmiar %zd bajtow\n", FLT, sizeof(float));
+	printf("Typ %9s ma rozmiar %zd bajtow\n", DBL, sizeof(double));
+	puts("");
 
 	// maksymalne wartości różnych typów
-	printf("Maksymalna wartosc typu char\t\twynosi %d.\n", CHAR_MAX);
-	printf("Maksymalna wartosc typu short\t\twynosi %hd.\n", SHRT_MAX);
-	printf("Maksymalna wartosc typu int\t\twynosi %d.\n", INT_MAX);
-	printf("Maksymalna wartosc typu unsigned\twynosi %u.\n", UINT_MAX);
-	printf("Maksymalna wartosc typu long\t\twynosi %ld.\n", LONG_MAX);
-	printf("Maksymalna wartosc typu long long\twynosi %lld.\n", LLONG_MAX);
-	printf("Maksymalna wartosc typu float\t\twynosi %e.\n", FLT_MAX);
-	printf("Maksymalna wartosc typu double\t\twynosi %e.\n\n", DBL_MAX);
-	
+	printf("Maksymalna wartosc typu %9s wynosi %d\n", C, CHAR_MAX);
+	printf("Maksymalna wartosc typu %9s wynosi %hd\n", S, SHRT_MAX);
+	printf("Maksymalna wartosc typu %9s wynosi %d\n", I, INT_MAX);
+	printf("Maksymalna wartosc typu %9s wynosi %u\n", U,  UINT_MAX);
+	printf("Maksymalna wartosc typu %9s wynosi %ld\n", L, LONG_MAX);
+	printf("Maksymalna wartosc typu %9s wynosi %lld\n", LL, LLONG_MAX);
+	printf("Maksymalna wartosc typu %9s wynosi %e\n", FLT, FLT_MAX);
+	printf("Maksymalna wartosc typu %9s wynosi %e\n", DBL, DBL_MAX);
+	puts("");
+
 	// nadanie zmiennym maksymalnej wartości
 	char_var = CHAR_MAX;
 	short_var = SHRT_MAX;
@@ -49,8 +62,8 @@ int main(void)
 	long_var = LONG_MAX;
 	long_long_var = LLONG_MAX;
 	double_var = DBL_MAX;
-	printf("Nadanie zmiennym maksymalnej wartosci\nStan programu: \nc \t%#c (%d), \ns \t%hd, \ni \t%d, \nu \t%u, \nlng \t%ld, \nlnglng \t%lld, \ndbl \t%e\n\n",
-		char_var, char_var, short_var, int_var, unsigned_var, long_var, long_long_var, double_var);
+	printf("Nadanie zmiennym maksymalnej wartosci\nStan programu: \nc \t%#4c (%4d), \ns \t%11hd, \ni \t%11d, \nu \t%11u, \nlng \t%11ld, \nlnglng \t%20lld, \ndbl \
+\t%14e\n\n", char_var, char_var, short_var, int_var, unsigned_var, long_var, long_long_var, double_var);
 
 	// próba przekroczenia limitu
 	char_var = char_var + 1;
@@ -60,28 +73,28 @@ int main(void)
 	long_var = long_var + 1;
 	long_long_var = long_long_var + 1;
 	double_var = double_var + 0.000001e+308;
-	printf("Proba przekroczenia limitu\nStan programu: \nc \t%#c (%d), \ns \t%hd, \ni \t%d, \nu \t%u, \nlng \t%ld, \nlnglng \t%lld, \ndbl \t%e\n\n",
-		char_var, char_var, short_var, int_var, unsigned_var, long_var, long_long_var, double_var);
+	printf("Proba przekroczenia limitu\nStan programu: \nc \t%#4c (%4d), \ns \t%11hd, \ni \t%11d, \nu \t%11u, \nlng \t%11ld, \nlnglng"
+			"\t%20lld, \ndbl \t%4e\n\n",char_var, char_var, short_var, int_var, unsigned_var, long_var, long_long_var, double_var);
 
-	char_var1 = 1327;
+	char_var1 = 1323;
 	short_var1 = 33768;
 	int_var1 = 2247483648;
 	long_var1 = 222147483648;
 
 	// przepełnienie
 	puts("Proba przepelnienia");
-	printf("char_var1 \t= %u, \t\tlecz nie %#c\nshort_var1 \t= %u, \t\tlecz nie %hd\nint_var1 \t= %u, \t\tlecz nie %d\nlong_var1 \t= %lld, \tlecz nie %ld\n\n",
+	printf("char_var1 \t= %12u, lecz nie %#c\nshort_var1 \t= %12u, lecz nie %hd\nint_var1 \t= %12u, lecz nie %d\nlong_var1 \t= %12lld, lecz nie %ld\n\n",
 		char_var1, char_var1, short_var1, short_var1, int_var1, int_var1, long_var1,long_var1);
 
 	//niedomiar zmiennej zmiennoprzecinkowej
 	puts("BADANIE NIEDOMIARU ZMIENNYCH ZMIENNOPRZECINKOWYCH:");
-	printf("FLT_MANT_DIG = \t\t%d\n", FLT_MANT_DIG);
-	printf("FLT_DIG = \t\t%d\n", FLT_DIG);
-	printf("FLT_MIN_10_EXP = \t%d\n", FLT_MIN_10_EXP);
-	printf("FLT_MAX_10_EXP = \t%d\n", FLT_MAX_10_EXP);
-	printf("FLT_MIN = \t\t%e\n", FLT_MIN);
-	printf("FLT_MAX = \t\t%e\n", FLT_MAX);
-	printf("FLT_EPSILON = \t\t%e\n\n", FLT_EPSILON);
+	printf("FLT_MANT_DIG \t= %3d\n", FLT_MANT_DIG);
+	printf("FLT_DIG \t= %3d\n", FLT_DIG);
+	printf("FLT_MIN_10_EXP \t= %3d\n", FLT_MIN_10_EXP);
+	printf("FLT_MAX_10_EXP \t= %3d\n", FLT_MAX_10_EXP);
+	printf("FLT_MIN \t= %e\n", FLT_MIN);
+	printf("FLT_MAX \t= %e\n", FLT_MAX);
+	printf("FLT_EPSILON \t= %e\n\n", FLT_EPSILON);
 
 	float_var1 =  1.175494e-038;
 
@@ -96,13 +109,13 @@ int main(void)
 	
 
 	puts("\nA teraz DOUBLE:");
-	printf("DBL_MANT_DIG = \t\t%d\n", DBL_MANT_DIG);
-	printf("DBL_DIG = \t\t%d\n", DBL_DIG);
-	printf("DBL_MIN_10_EXP = \t%d\n", DBL_MIN_10_EXP);
-	printf("DBL_MAX_10_EXP = \t%d\n", DBL_MAX_10_EXP);
-	printf("DBL_MIN = \t\t%e\n", DBL_MIN);
-	printf("DBL_MAX = \t\t%e\n", DBL_MAX);
-	printf("DBL_EPSILON = \t\t%e\n\n", DBL_EPSILON);
+	printf("DBL_MANT_DIG \t= %4d\n", DBL_MANT_DIG);
+	printf("DBL_DIG \t= %4d\n", DBL_DIG);
+	printf("DBL_MIN_10_EXP \t= %4d\n", DBL_MIN_10_EXP);
+	printf("DBL_MAX_10_EXP \t= %4d\n", DBL_MAX_10_EXP);
+	printf("DBL_MIN \t= %e\n", DBL_MIN);
+	printf("DBL_MAX \t= %e\n", DBL_MAX);
+	printf("DBL_EPSILON \t= %e\n\n", DBL_EPSILON);
 
 	double_var1 =  2.225074e-308;
 
