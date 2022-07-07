9d580827d65e5e840279a8b3baef8abf5c3f8e32
LiubomirBezgan <106992442+LiubomirBezgan@users.noreply.github.com>
2022-07-07 18:20:28 +0200

    setlocale()

--- Chapter 5/Listing_5_2.c
+++ Chapter 5/Listing_5_2.c
@@ -1,11 +1,15 @@
-// opis - Exercise?Chapter?.c
-// v1.0
+// scale - Listing_5_2.c
+// v2.0 - uzycie funkcji lokalizacji
 #include <stdio.h>
 #define KOREKTA 32.0 //jeden rodzaj stalej
+#include <locale.h>
 
 int main(void)
 {
-	const double MNOZNIK = 1.8; // drugi rodzaj stalej
+	setlocale(LC_ALL, ""); 		// funkcja lokalizacji (zmienia "." na ",")
+	const double MNOZNIK = 1.8;	// drugi rodzaj stalej
+	const char DEG = -80;		/* dla stanu domyślnego (bez uzycia funkcji lokalizacji)
+								   symbolowi stopnia (°) odpowiada wartość liczbowa -8 */ 
 	double deg_celsius, deg_fahrenheit;
 	printf("Stopni Celsjusza\tStopni Fahrenheita\n");
 	deg_celsius = -10.0;
@@ -13,7 +17,7 @@ int main(void)
 	while (deg_celsius < 32.0)
 		{
 			deg_fahrenheit = MNOZNIK * deg_celsius + KOREKTA;
-			printf("%9.1f C \t\t%9.1f F\n", deg_celsius, deg_fahrenheit);
+			printf("%9.1f %cC \t\t%9.1f %cF\n", deg_celsius, DEG, deg_fahrenheit, DEG);
 			puts("------------------------------------------");
 			deg_celsius = deg_celsius +5.0;
 		}
