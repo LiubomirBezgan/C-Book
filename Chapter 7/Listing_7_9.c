22df8090ebc5f593a82cc0ec0c787059d3901e0d
LiubomirBezgan <106992442+LiubomirBezgan@users.noreply.github.com>
2022-08-05 22:27:42 +0200

    Chapter 7: ?:, continue

---
+++ Chapter 7/Listing_7_9.c
@@ -0,0 +1,44 @@
+// Program wykorzystuje continue do pominiecia czesci petli - Listing_7_9.c
+// v1.0
+#include <stdio.h>
+
+int main(void)
+{
+	const float MIN = 0.0f;
+	const float MAX = 100.0f;
+
+	float wynik;
+	float suma = 0.0f;
+	int n = 0;
+	float min = MAX;
+	float max = MIN;
+
+	puts("Podaj pierwszy wynik (albo 'q' zeby zakonczyc):");
+	while (scanf("%f", &wynik) == 1)
+	{
+		if (wynik < MIN || wynik > MAX)
+		{
+			printf("%0.1f jest nieprawidlowa wartoscia.\n", wynik);
+			continue;				// skok do warunku wykonania petli
+		}
+		printf("Przyjeto: %0.1f\n", wynik);
+		min = (wynik < min)? wynik : min;
+		max = (wynik > max)? wynik : max;
+		suma += wynik;
+		n++;
+		puts("Podaj kolejny wynik (albo 'q' zeby zakonczyc):");
+	}
+	
+	if (n>0)
+	{
+		printf("Srednia z %d wynikow wynosi %0.1f.\n", n, suma/n);
+		printf("Najnizszy = %0.1f, najwyzszy = %0.1f\n", min, max);
+	}
+	else
+		puts("Nie podano zadnych prawidlowych wynikow.");
+
+	getchar();
+	getchar();
+	getchar();
+	return 0;
+}
