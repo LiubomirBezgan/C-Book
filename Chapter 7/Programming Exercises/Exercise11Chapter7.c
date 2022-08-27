3be8744114f4d4f6ace269e52bd45e94249cf021
LiubomirBezgan <106992442+LiubomirBezgan@users.noreply.github.com>
2022-08-27 22:30:51 +0200

    Chapter 7: HW VI

--- Chapter 7/Programming Exercises/Exercise11Chapter7.c
+++ Chapter 7/Programming Exercises/Exercise11Chapter7.c
@@ -10,7 +10,7 @@
 #define RABAT 0.05					// rabatem w wysokosci 5% naliczanym przed dodaniem kosztow przesylki.
 #define PROG1 5 					// kg - Pierwszy prog wagi przesylki.
 #define PROG2 20 					// kg - Drugi prog wagi przesylki.
-#define KOSZT0 3,5 					// zl - Koszt przesylwki o wadze nieprzekraczajacej PROG1
+#define KOSZT0 3.5 					// zl - Koszt przesylwki o wadze nieprzekraczajacej PROG1
 #define KOSZT1 10 					// zl - Koszt przesylwki o wadze mieszcacej sie pomiedzy PROG1 a PROG2
 #define KOSZT2 8 					// zl + 0.1 zl za kazdy kilogram - Koszt przesylwki o wadze przekraczajacej PROG2
 #define KOSZT_DODATKOWY 0.1 		// 0.1 zl za kazdy kilogram
@@ -21,7 +21,8 @@ int main(void)
 /*  Variables -----------------------------------------------------------------*/
 	bool symbol = false;			// flaga: czy zostal wprowadzony wlasciwy symbol?
 	bool koniec = false;			// flaga: czy nalezy skonczyc przyjmowanie zamowienia?
-	float waga_arbuzy=0, waga_buraki=0, waga_cebula=0;
+	float waga_arbuzy=0, waga_buraki=0, waga_cebula=0, waga_calosc;
+	float wartosc_arbuzy, wartosc_buraki, wartosc_cebula, wartosc_calosc;
 	float zamawiane_kg, do_zaplaty, rabat=0, koszt_przesylki;
 
 	
@@ -77,19 +78,30 @@ int main(void)
 					continue;		                            // czyszczenie bufora
 	    	}
 	    	if (!koniec)
-	    		puts("Podaj kolejna litere");
+	    		puts("\nPodaj kolejna litere");
 	    	symbol = false;
 	    }
 	    // obliczenia
-	    if (waga_arbuzy * CENA_ARBUZY + waga_buraki * CENA_BURAKI + waga_cebula * CENA_CEBULA > PROG_ZAMOWIENIE)
-	    	rabat = (waga_arbuzy * CENA_ARBUZY + waga_buraki * CENA_BURAKI + waga_cebula * CENA_CEBULA) * RABAT;
-	    koszt_przesylki = KOSZT2 + KOSZT_DODATKOWY * (waga_arbuzy + waga_buraki + waga_cebula);
-	    do_zaplaty = koszt_przesylki + ((waga_arbuzy * CENA_ARBUZY + waga_buraki * CENA_BURAKI + waga_cebula * CENA_CEBULA) - rabat);
+	    waga_calosc = waga_arbuzy + waga_buraki + waga_cebula;
+	    wartosc_arbuzy = waga_arbuzy * CENA_ARBUZY;
+	    wartosc_buraki = waga_buraki * CENA_BURAKI;
+	    wartosc_cebula = waga_cebula * CENA_CEBULA;
+	    wartosc_calosc = wartosc_arbuzy + wartosc_buraki + wartosc_cebula;
+	    if (wartosc_calosc > PROG_ZAMOWIENIE)
+	    	rabat = wartosc_calosc * RABAT;
+	    if (waga_calosc > 20)
+	        koszt_przesylki = KOSZT2 + KOSZT_DODATKOWY * (waga_arbuzy + waga_buraki + waga_cebula);
+	    else if (waga_calosc > 5)
+	        koszt_przesylki = KOSZT1;
+	    else
+	        koszt_przesylki = KOSZT0;
+	   
+	    do_zaplaty = koszt_przesylki + (wartosc_calosc - rabat);
 	    // wyświetlenie wyniku
-	    printf("Cena jednastkowa za kilogram: %.2f zl/kg\n", (waga_arbuzy * CENA_ARBUZY + waga_buraki * CENA_BURAKI + waga_cebula * CENA_CEBULA)/(waga_arbuzy + waga_buraki + waga_cebula));
+	    printf("Ceny jednastkowe za kilogram: arbuzy - %.2f kg, buraki - %.2f kg, cebula - %.2f kg\n", CENA_ARBUZY, CENA_BURAKI, CENA_CEBULA);
 	    printf("Ilosc zamowionych warzyw: arbuzy - %.2f kg, buraki - %.2f kg, cebula - %.2f kg\n", waga_arbuzy, waga_buraki, waga_cebula);
-	    printf("Wartosc dla kazdego z zamowionych towarow: arbuzy - %.2f zl, buraki - %.2f zl, cebula - %.2f zl\n", waga_arbuzy * CENA_ARBUZY, waga_buraki * CENA_BURAKI, waga_cebula * CENA_CEBULA);
-	    printf("Wartosc calosci zamowienia: %.2f zl\n", waga_arbuzy * CENA_ARBUZY + waga_buraki * CENA_BURAKI + waga_cebula * CENA_CEBULA);
+	    printf("Wartosc dla kazdego z zamowionych towarow: arbuzy - %.2f zl, buraki - %.2f zl, cebula - %.2f zl\n", wartosc_arbuzy, wartosc_buraki, wartosc_cebula);
+	    printf("Wartosc calosci zamowienia: %.2f zl\n", wartosc_calosc);
 	    printf("Wysokosc przyslugujacego rabatu: %.2f zl\n", rabat);
 	    printf("Koszt przesylki: %.2f zl\n", koszt_przesylki);
 	    printf("Calkowity koszt zamowienia: %.2f zl\n", do_zaplaty);
